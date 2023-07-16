d0e2a9eecd05a88a529a2dc3627274633e71f72a
Kitwana Akil <kakil@me.com>
2023-07-16 16:07:17 -0400

    Added json files to project.  Added images to project.  Implemented astronaut data structure and loading/decoding astronaut json.

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Kitwana Akil on 7/16/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+    
+}
