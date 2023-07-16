ea23962efd4a15bc29d13a146cfb7c21c0a0ad7e
Kitwana Akil <kakil@me.com>
2023-07-16 15:37:23 -0400

    completed scrolling grid layout.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+//        GridItem(.fixed(80)),
+//        GridItem(.fixed(80)),
+//        GridItem(.fixed(80))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
+                }
+            }
         }
         .padding()
     }
