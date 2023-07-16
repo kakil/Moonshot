6d7941e881f65a043a8d4c213ab277f7b0eafd3c
Kitwana Akil <kakil@me.com>
2023-07-16 14:43:44 -0400

    completed navigation link tutorial 3

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink {
+                    Text("Detail \(row)")
+                        .padding()
+                } label: {
+                    Text("Row \(row)")
+                }
+            .navigationTitle("SwiftUI")
+            }
         }
-        .padding()
     }
 }
 
